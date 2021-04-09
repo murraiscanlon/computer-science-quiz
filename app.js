102d6a992bfbdf85cb40b4aabd0d7c09eeee33f6
✰ Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2021-04-08 19:39:30 -0500

    update again

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-window.onload = sendApiRequest()
+window.onload = getApiCategories()
 
+let testhome = 'hello from testhome';
+console.log(testhome);
 
-//An asynchronous function to fetch data from the API
-async function sendApiRequest(){
+//1.
+//An asynchronous function to fetch CATEGORY data from the API for the dropdown list on the landing screen
+async function getApiCategories(){
     let response = await fetch(`https://opentdb.com/api_category.php`);
     // console.log(response)
 
     let data = await response.json()
     // console.log(data)
 
-    getCategories(data)
+    length = data.trivia_categories.length;
+    let categories = document.getElementById("categories");
 
+    let testCrossOver = [];
+    for (let i = 0; i < length; i++){
+        let option = document.createElement("OPTION"),
+            txt = document.createTextNode(`${data.trivia_categories[i].id - 8}. ${data.trivia_categories[i].name}`);
+            testCrossOver.push(data.trivia_categories[i].id);
+        option.appendChild(txt);
+        categories.insertBefore(option, categories.lastChild);
+    }
 
-
+    let demoreturn = crossover(testCrossOver);
+    console.log("demoreturn: " + demoreturn);
 
 }
 
-function getCategories(data){
-
-
-    let select = $('#categories');
-    select.html('');
-    length = data.trivia_categories.length;
-    for (let i = 0; i < length; i++) { //starts loop
-        select.append('<option id="' + data.trivia_categories[i].name + '">' + data.trivia_categories[i].name + '</option>');
-        // console.log("category: " + data.trivia_categories[i].name); //What ever you want
-    } //ends loop
-//lksdjflskdfj
-}//end
-
-
+© 2021 GitHub, Inc.
