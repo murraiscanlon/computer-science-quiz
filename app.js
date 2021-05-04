e142468f0d162f7c7cc38113205496059063cb14
murrai <murrai.scanlon@gmail.com>
2021-05-04 15:06:21 -0500

    stacked buttons on landing page

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 window.onload = getApiCategories()
 
-let testhome = 'hello from testhome';
-console.log(testhome);
 
-//1.
 //An asynchronous function to fetch CATEGORY data from the API for the dropdown list on the landing screen
 async function getApiCategories(){
     let response = await fetch(`https://opentdb.com/api_category.php`);
@@ -15,17 +12,13 @@ async function getApiCategories(){
     length = data.trivia_categories.length;
     let categories = document.getElementById("categories");
 
-    let testCrossOver = [];
     for (let i = 0; i < length; i++){
         let option = document.createElement("OPTION"),
+            //category ids start at 8, so subtract 8 to get the display starting at #1
             txt = document.createTextNode(`${data.trivia_categories[i].id - 8}. ${data.trivia_categories[i].name}`);
-            testCrossOver.push(data.trivia_categories[i].id);
         option.appendChild(txt);
         categories.insertBefore(option, categories.lastChild);
     }
-
-    let demoreturn = crossover(testCrossOver);
-    console.log("demoreturn: " + demoreturn);
 
 }
 
